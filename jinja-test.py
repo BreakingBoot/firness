f220622a3fbcc8b55a81769db74d59b2a6d79a19
cglosner <cglosner@gmail.com>
2023-10-23 09:40:57 -0400

    [Fix] It now handles gather fuzzable parameters, along with known constants

--- jinja-test.py
+++ jinja-test.py
@@ -6,8 +6,8 @@ from typing import List, Dict, Any
 
 
 scalable_params = [
-    "INT",
-    "CHAR"
+    "int",
+    "char"
 ]
 
 class Argument:
@@ -24,6 +24,11 @@ class FunctionBlock:
         self.arguments = arguments
         self.function = function
 
+#
+# Load in the function call database and perform frequency analysis across
+# the function calls to make sure to only keep the function calls that have
+# the same type of input args
+#
 def load_data(json_file: str) -> Dict[str, List[FunctionBlock]]:
     with open(json_file, 'r') as file:
         raw_data = json.load(file)
@@ -47,6 +52,36 @@ def load_data(json_file: str) -> Dict[str, List[FunctionBlock]]:
     filtered_function_dict = {function: [fb for fb in function_blocks if len(fb.arguments) == most_common_param_counts[function]]
                               for function, function_blocks in function_dict.items()}
 
+    void_star_data_type_counter = defaultdict(Counter)
+
+    # Step 1: Collect data_type statistics for arg_type of "void *"
+    for function, function_blocks in filtered_function_dict.items():
+        for function_block in function_blocks:
+            for arg_key, argument in function_block.arguments.items():
+                if contains_void_star(argument.arg_type):
+                    void_star_data_type_counter[arg_key].update([argument.data_type])
+
+    # Step 2: Determine dominant data_type for each arg_key
+    dominant_data_types = {}
+    for arg_key, data_type_counter in void_star_data_type_counter.items():
+        total = sum(data_type_counter.values())
+        for data_type, count in data_type_counter.items():
+            if count / total >= 0.9:
+                dominant_data_types[arg_key] = data_type
+                break
+
+    # Step 3: Filter out FunctionBlock instances with differing data_type for dominant arg_keys
+    for function, function_blocks in filtered_function_dict.items():
+        filtered_function_dict[function] = [
+            fb for fb in function_blocks
+            if all(
+                arg_key not in dominant_data_types or
+                argument.data_type == dominant_data_types[arg_key]
+                for arg_key, argument in fb.arguments.items()
+                if contains_void_star(argument.arg_type)
+            )
+        ]
+
     return filtered_function_dict
 
 class FieldInfo:
@@ -59,7 +94,9 @@ def object_to_dict(obj):
         return obj 
     return obj.__dict__
 
-
+#
+# Load in the structures
+#
 def load_types(json_file: str) -> Dict[str, List[FieldInfo]]:
     with open(json_file, 'r') as file:
         data = json.load(file)
@@ -72,21 +109,10 @@ def load_types(json_file: str) -> Dict[str, List[FieldInfo]]:
         type_data_list[type_data_dict['TypeName']] = fields_list
     return type_data_list
 
-# def pre_process_data(function_dict: Dict[str, List[FunctionBlock]]) -> Dict[str, Dict[str, List[Argument]]]:
-#     filtered_args_dict = defaultdict(lambda: defaultdict(list))
-#     usage_seen = defaultdict(lambda: defaultdict(set))  # Keeps track of arg.usage values seen for each function and arg_key
-
-#     for function, function_blocks in function_dict.items():
-#         for function_block in function_blocks:
-#             for arg_key, argument in function_block.arguments.items():
-#                 if argument.arg_dir == "IN" and (argument.variable == "__CONSTANT_STRING__" or argument.variable == "__CONSTANT_INT__"):
-#                     # Check if arg.usage is already in the set for this function and arg_key
-#                     if argument.usage not in usage_seen[function][arg_key]:
-#                         filtered_args_dict[function][arg_key].append(argument)
-#                         usage_seen[function][arg_key].add(argument.usage)  # Update the set with the new arg.usage value
-
-#     return filtered_args_dict
-
+#
+# Filters out any arguments that are not IN and CONSTANT values, but making sure
+# to not keep duplicates
+#
 def pre_process_data(function_dict: Dict[str, List[FunctionBlock]]) -> Dict[str, Dict[str, List[Argument]]]:
     filtered_args_dict = defaultdict(lambda: defaultdict(list))
     collected_args_dict = defaultdict(lambda: defaultdict(list))  # Temporary storage for collected arguments
@@ -112,6 +138,48 @@ def pre_process_data(function_dict: Dict[str, List[FunctionBlock]]) -> Dict[str,
 
     return filtered_args_dict
 
+def contains_void_star(s):
+    s_no_spaces = s.replace(" ", "").lower()
+    return "void*" in s_no_spaces
+
+# 
+# Get fuzzable saves the arguments that take scalar inputs and also saves void * inputs that vary argument type
+# by more than 5 since the assumption is that means the function is most likely manipulating data, not structures
+# themselves
+# 
+def get_fuzzable(function_dict: Dict[str, List[FunctionBlock]]) -> Dict[str, List[FunctionBlock]]:
+    filtered_args_dict = defaultdict(list)
+
+    for function, function_blocks in function_dict.items():
+        void_star_data_type_counter = defaultdict(Counter)
+
+        for function_block in function_blocks:
+            for arg_key, argument in function_block.arguments.items():
+                if contains_void_star(argument.arg_type):
+                    void_star_data_type_counter[arg_key].update([argument.data_type])
+        
+        for function_block in function_blocks:
+            new_arguments = {}
+
+            for arg_key, argument in function_block.arguments.items():
+                arg_type = argument.arg_type.lower()
+                is_scalable = any(param.lower() in arg_type for param in scalable_params)
+                if argument.arg_dir == "IN" and is_scalable and (argument.variable != "__CONSTANT_INT__" and argument.variable != "__CONSTANT_STRING__"):
+                    scalable_arg = Argument(argument.arg_dir, argument.arg_type, "", argument.data_type, argument.usage, argument.variable)
+                    new_arguments[arg_key] = scalable_arg
+                elif contains_void_star(argument.arg_type) and len(void_star_data_type_counter[arg_key]) > 5:
+                    scalable_arg = Argument(argument.arg_dir, argument.arg_type, "", argument.data_type, argument.usage, argument.variable)
+                    new_arguments[arg_key] = scalable_arg
+
+
+            # Create a new FunctionBlock with the filtered arguments and append it to filtered_args_dict[function]
+            if new_arguments:
+                new_function_block = FunctionBlock(new_arguments, function_block.function)
+                filtered_args_dict[function].append(new_function_block)
+                break
+
+    return filtered_args_dict
+
 
 def generate_code(function_dict: Dict[str, List[FunctionBlock]], types_dict: Dict[str, List[FieldInfo]]):
     env = Environment(loader=FileSystemLoader('.'))
@@ -124,16 +192,34 @@ def write_to_file(data: Dict[str, Dict[str, List[Argument]]], file_path: str):
     with open(file_path, 'w') as file:
         json.dump(serializable_data, file, indent=4)
 
+def print_filtered_args_dict(filtered_args_dict: Dict[str, List[FunctionBlock]]) -> None:
+    for function, function_blocks in filtered_args_dict.items():
+        print(f'Function: {function}')
+        for idx, function_block in enumerate(function_blocks, 1):
+            for arg_key, argument in function_block.arguments.items():
+                print(f'    Argument Key: {arg_key}')
+                print(f'      Arg Dir: {argument.arg_dir}')
+                print(f'      Arg Type: {argument.arg_type}')
+                print(f'      Assignment: {argument.assignment}')
+                print(f'      Data Type: {argument.data_type}')
+                print(f'      Usage: {argument.usage}')
+                print(f'      Variable: {argument.variable}')
+
+
 def main():
     parser = argparse.ArgumentParser(description='Process some data.')
     parser.add_argument('-d', '--data-file', dest='data_file', default='tmp/call-database.json', help='Path to the data file (default: tmp/call-database.json)')
+    parser.add_argument('-g', '--generator-file', dest='generator_file', default='tmp/generator-database.json', help='Path to the generator file (default: tmp/generator-database.json)')
     parser.add_argument('-t', '--types-file', dest='types_file', default='tmp/types.json', help='Path to the types file (default: tmp/types.json)')
     parser.add_argument('-o', '--output-file', dest='output_file', default='output.json', help='Path to the output file (default: output.json)')
 
     args = parser.parse_args()
 
+    generators = load_data(args.generator_file)
     data = load_data(args.data_file)
     pre_processed_data = pre_process_data(data)
+    directly_fuzzable = get_fuzzable(data)
+    print_filtered_args_dict(directly_fuzzable)
     types = load_types(args.types_file)
     write_to_file(pre_processed_data, args.output_file)
 
